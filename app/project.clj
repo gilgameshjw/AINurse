6bb6695f91fc763d4cda482cad5ba7b4b7f9c56d
jair <jair@debian>
2019-04-01 23:58:10 +0100

    added reagent/figwheel

--- app/project.clj
+++ app/project.clj
@@ -1,7 +1,46 @@
 (defproject app "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns app.core})
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.946"]
+                 [reagent "0.7.0"]]
+
+  :min-lein-version "2.5.3"
+
+  :source-paths ["src/clj"]
+
+  :plugins [[lein-cljsbuild "1.1.4"]]
+
+  :clean-targets ^{:protect false} ["resources/public/js"
+                                    "target"]
+
+  :figwheel {:css-dirs ["resources/public/css"]}
+
+  :profiles
+  {:dev
+   {:dependencies [[figwheel-sidecar "0.5.13"]]
+
+    :plugins      [[lein-figwheel "0.5.15"]]
+    }}
+
+  :cljsbuild
+  {:builds
+   [{:id           "dev"
+     :source-paths ["src/cljs"]
+     :figwheel     {:on-jsload "app.core/reload"}
+     :compiler     {:main                 app.core
+                    :optimizations        :none
+                    :output-to            "resources/public/js/app.js"
+                    :output-dir           "resources/public/js/dev"
+                    :asset-path           "js/dev"
+                    :source-map-timestamp true}}
+
+    {:id           "min"
+     :source-paths ["src/cljs"]
+     :compiler     {:main            app.core
+                    :optimizations   :advanced
+                    :output-to       "resources/public/js/app.js"
+                    :output-dir      "resources/public/js/min"
+                    :elide-asserts   true
+                    :closure-defines {goog.DEBUG false}
+                    :pretty-print    false}}
+
+    ]})
